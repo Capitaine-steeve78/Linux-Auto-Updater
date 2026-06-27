2587944d462f9dd5f27ba8ea51851f5956700ff3
Capitaine-steeve78 <238971364+Capitaine-steeve78@users.noreply.github.com>
2026-06-27 18:32:50 +0200

    test 4.2.1

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import subprocess
 import logging
+import os
 
 logging.basicConfig(level=logging.INFO)
+
 
 def run_command(command):
     try:
@@ -12,16 +14,30 @@ def run_command(command):
             capture_output=True,
             text=True
         )
-        print(f"{command}")
+        print(command)
         print(result.stdout)
         return True
+
     except subprocess.CalledProcessError as e:
         print(f"ERROR: {command}, {e.stderr.strip()}")
         return False
 
+
+# =========================
+# GESTION SUDO AUTOMATIQUE
+# =========================
+def run_sudo_command(command):
+    # si root → on enlève sudo
+    if os.geteuid() == 0:
+        command = command.replace("sudo ", "")
+
+    return run_command(command)
+
+
 # Exemple d'utilisation
 if __name__ == "__main__":
-    if not run_command("sudo apt update && sudo apt upgrade -y"):
+
+    if not run_sudo_command("sudo apt update && sudo apt upgrade -y"):
         print("ERROR")
         # logging.error("Failed to install sudo apt update")
     # run_command("sudo apt upgrade -y")
