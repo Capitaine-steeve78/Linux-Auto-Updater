414a75697606fbda70d82f563608d0ac43a77d19
Capitaine-steeve78 <238971364+Capitaine-steeve78@users.noreply.github.com>
2026-06-27 18:28:15 +0200

    test 4.2

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+PROJECT_DIR="/opt/linux-auto-updater"
+
+cd "$PROJECT_DIR" || {
+  echo "ERROR: project directory not found: $PROJECT_DIR"
+  exit 1
+}
+
 export PATH="$HOME/.local/bin:$PATH"
 
 uv run python main.py
