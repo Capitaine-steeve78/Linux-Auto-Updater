a02a94b8a0ceccda48c65af8cde419190d5128c4
Capitaine-steeve78 <238971364+Capitaine-steeve78@users.noreply.github.com>
2026-06-27 16:51:05 +0200

    test 2.2

--- scripts/config.sh
+++
@@ -1,7 +0,0 @@
-CONFIG_FILE="/etc/linux-auto-updater/config.yaml"
-
-load_config() {
-  INSTALL_DIR=$(grep "^install_dir:" "$CONFIG_FILE" | awk '{print $2}')
-  INTERVAL_MINUTES=$(grep "^interval_minutes:" "$CONFIG_FILE" | awk '{print $2}')
-  BRANCH=$(grep "^branch:" "$CONFIG_FILE" | awk '{print $2}')
-}
--- scripts/git.sh
+++
@@ -1,6 +0,0 @@
-check_git_signature() {
-  git verify-commit HEAD >/dev/null 2>&1 || {
-    error "Unsigned commit detected"
-    exit 1
-  }
-}
