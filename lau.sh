37f345a465a5a8d131ddc31526b584f872796f5b
Capitaine-steeve78 <238971364+Capitaine-steeve78@users.noreply.github.com>
2026-06-27 18:16:46 +0200

    test 4.1

--- lau.sh
+++ lau.sh
@@ -1,29 +1,22 @@
 #!/bin/bash
 
-# =========================
-# SCRIPT LAUNCHER
-# =========================
-
 SCRIPT_NAME="$1"
 
-# Vérifie si un argument est fourni
 if [ -z "$SCRIPT_NAME" ]; then
   echo "ERROR : no script specified"
   exit 1
 fi
 
-# Dossier scripts (au même niveau que ce script)
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)/scripts"
+# CHEMIN FIXE DU PROJET (IMPORTANT)
+PROJECT_DIR="/opt/linux-auto-updater"
+SCRIPT_DIR="$PROJECT_DIR/scripts"
 
-# Transforme "exemple/test" en chemin réel
 TARGET_SCRIPT="$SCRIPT_DIR/$SCRIPT_NAME.sh"
 
-# Vérifie si le fichier existe
 if [ -f "$TARGET_SCRIPT" ]; then
   bash "$TARGET_SCRIPT"
   exit 0
 fi
 
-# Si pas trouvé → erreur claire
 echo "ERROR : script '$SCRIPT_NAME.sh' not found in $SCRIPT_DIR"
 exit 1
